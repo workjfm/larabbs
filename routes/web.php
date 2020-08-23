502deab828a28acac88264c7e091611f8669539d
jfm <workjfm142837@163.com>
2020-08-23 21:17:23 +0800

    用户个人页面原型

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes(['verify' => true]);
 
+// 用户
+Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
