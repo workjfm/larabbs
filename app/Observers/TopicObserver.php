c48e9ff2005d54d033018d0c906c1a8d79d8b836
jfm <workjfm142837@163.com>
2020-08-24 11:52:52 +0800

    用户可以删除自己发的话题

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -16,9 +16,4 @@ class TopicObserver
 
         $topic->excerpt = make_excerpt($topic->body);
     }
-
-    public function update(User $user, Topic $topic)
-    {
-        return $topic->user_id == $user->id;
-    }
 }
