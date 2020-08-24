ffb1f0636011862183e9b65ebdafb61307089c19
jfm <workjfm142837@163.com>
2020-08-24 10:33:37 +0800

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,5 @@ Auth::routes(['verify' => true]);
 
 // 用户
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
