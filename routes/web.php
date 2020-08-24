c10c31a67e22a3fc4dd47e892cf7e84cbc255e6f
jfm <workjfm142837@163.com>
2020-08-24 20:57:12 +0800

    翻译 URL 标识

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,11 @@ Auth::routes(['verify' => true]);
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 // 帖子
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
 // 分类
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
