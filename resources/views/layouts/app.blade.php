259602df14414f5a62e908818b4e6f08f1c1bc6d
jfm <workjfm142837@163.com>
2020-08-24 11:05:07 +0800

    话题排序

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>@yield('title', 'Jfmblog') - Jfmblog 首页</title>
+  <title>@yield('title', 'Jfmbbs') - Jfmbbs 首页</title>
 
   <!-- Styles -->
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
